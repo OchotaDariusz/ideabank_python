137420d4da25c5c52fe281b752bdfcd47c29e57c
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-03-05 15:17:00 +0100

    Added list ideas feature

--- ideabank.py
+++ ideabank.py
@@ -2,8 +2,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--list', type=int, required=False)
+parser.add_argument('--list', action='store_true', required=False)
 parser.add_argument('--delete', type=int, required=False)
+args = parser.parse_args()
 
 
 def open_ideas(file_name):
@@ -21,11 +22,16 @@ def export_ideas(file_name, elements):
 
 if __name__ == "__main__":
     ideas = open_ideas("ideas.txt")
-    print(ideas)
-    while True:
+    if not args.list and not args.delete:
+        while True:
+            if ideas:
+                for index in range(len(ideas)):
+                    print(f"{index + 1}. {ideas[index]}")
+            idea = input("What is your new idea?\n")
+            ideas.append(idea)
+            export_ideas("ideas.txt", ideas)
+
+    if args.list:
         if ideas:
             for index in range(len(ideas)):
                 print(f"{index + 1}. {ideas[index]}")
-        idea = input("What is your new idea?\n")
-        ideas.append(idea)
-        export_ideas("ideas.txt", ideas)
