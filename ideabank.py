f566ff6c5770761d06116302fb9f31de9ff2c982
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-03-05 14:59:58 +0100

    Add ideas feature

--- ideabank.py
+++ ideabank.py
@@ -1,1 +1,31 @@
-# Put your code here
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--list', type=int, required=False)
+parser.add_argument('--delete', type=int, required=False)
+
+
+def open_ideas(file_name):
+    with open(file_name, "r+") as file:
+        lines = file.readlines()
+    elements = [element.replace("\n", "").split(";") for element in lines]
+    return elements[0]
+
+
+def export_ideas(file_name, elements):
+    with open(file_name, "w") as file:
+        ideas = ';'.join(elements)
+        file.write(ideas + "\n")
+
+
+if __name__ == "__main__":
+    ideas = open_ideas("ideas.txt")
+    print(ideas)
+    while True:
+        if ideas:
+            for index in range(len(ideas)):
+                print(f"{index + 1}. {ideas[index]}")
+        idea = input("What is your new idea?\n")
+        ideas.append(idea)
+        export_ideas("ideas.txt", ideas)
