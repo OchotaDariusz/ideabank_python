a630c3ca5e5551f776313d79d95831a38a0ff028
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-03-10 10:07:55 +0100

    Fixed creating file if it doesn't exists

--- ideabank.py
+++ ideabank.py
@@ -2,10 +2,16 @@ import sys
 
 
 def open_ideas(file_name):
-    with open(file_name, "r+") as file:
-        lines = file.readlines()
-    elements = [element.replace("\n", "").split(";") for element in lines]
-    return elements[0]
+    try:
+        file = open(file_name, "r+").readlines()
+        elements = [element.replace("\n", "").split(";") for element in file]
+    except FileNotFoundError:
+        file = open(file_name, "w")
+        elements = list()
+    if elements:
+        return elements[0]
+    else:
+        return elements
 
 
 def export_ideas(file_name, elements):
@@ -15,36 +21,32 @@ def export_ideas(file_name, elements):
 
 
 if __name__ == "__main__":
-    try:
-        ideas = open_ideas("ideas.txt")
-    except IndexError:
-        ideas = list()
-    finally:
-        if len(sys.argv) == 1:
-            while True:
-                if ideas:
-                    for index in range(len(ideas)):
-                        print(f"{index + 1}. {ideas[index]}")
-                idea = input("What is your new idea?\n")
-                ideas.append(idea)
-                export_ideas("ideas.txt", ideas)
-
-        elif sys.argv[1] == "--list":
+    ideas = open_ideas("ideas.txt")
+    if len(sys.argv) == 1:
+        while True:
             if ideas:
                 for index in range(len(ideas)):
                     print(f"{index + 1}. {ideas[index]}")
+            idea = input("What is your new idea?\n")
+            ideas.append(idea)
+            export_ideas("ideas.txt", ideas)
+
+    elif sys.argv[1] == "--list":
+        if ideas:
+            for index in range(len(ideas)):
+                print(f"{index + 1}. {ideas[index]}")
+        else:
+            print("There's no ideas!")
+
+    elif sys.argv[1] == "--delete":
+        try:
+            if ideas:
+                try:
+                    ideas.pop(int(sys.argv[2]) - 1)
+                    export_ideas("ideas.txt", ideas)
+                except IndexError:
+                    raise ValueError
             else:
                 print("There's no ideas!")
-
-        elif sys.argv[1] == "--delete":
-            try:
-                if ideas:
-                    try:
-                        ideas.pop(int(sys.argv[2]) - 1)
-                        export_ideas("ideas.txt", ideas)
-                    except IndexError:
-                        raise ValueError
-                else:
-                    print("There's no ideas!")
-            except ValueError:
-                print("Specify a number after --delete")
+        except ValueError:
+            print("Specify a number after --delete")
